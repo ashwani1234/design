a7123f78f8d60802e28fdb04063fbbdb25e88c67
Ashwani Kumar <ashwani@Ashwanis-MacBook-Pro.local>
2022-04-21 20:23:01 +0530

    changes

--- server.js
+++ server.js
@@ -7,28 +7,19 @@ const port = 2000
 
 app.use(express.static(path.join(__dirname, "../routes")))
 
-// app.get('/', function(req, res) {
-//     res.sendFile(path.join(__dirname, './index.html'));
-// });
-// app.get('/corona', function(req, res) {
-//     res.sendFile(path.join(__dirname, './corona.html'));
-// });
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, './index.html'));
+});
+app.get('/about', function(req, res) {
+    res.sendFile(path.join(__dirname, './about.html'));
+});
 
 // All requests to this router will
 // first hit this middleware
-router.use(function (req, res, next) {
-  console.log("Middleware Called");
-  next();
-})
-// Always invoked
-router.use(function (req, res, next) {
-    res.sendFile(path.join(__dirname, './index.html'));
-})
-  
-app.use('/about', router);
+
   
 app.listen(port, function(err){
     if (err) console.log(err);
     console.log("Server listening on PORT", port);
 });
-app.listen(2000, "0.0.0.0");
+
